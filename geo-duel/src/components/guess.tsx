c326c3f034e28dd434e66fd3ecb694b905dd253b
GSichelero <gabriel.sichelero@hotmail.com>
2022-03-23 14:28:30 -0300

    Change to guess phase

--- geo-duel/src/components/guess.tsx
+++ geo-duel/src/components/guess.tsx
@@ -42,58 +42,6 @@ const render = (status: Status): any => {
   return <h1>{status}</h1>;
 };
 
-// function MyMapComponent({
-//   center,
-//   zoom,
-// }: {
-//   center: google.maps.LatLngLiteral;
-//   zoom: number;
-// }) {
-//   const ref: any = useRef();
-
-//   useEffect(() => {
-//     new window.google.maps.Map(ref.current, {
-//       center,
-//       zoom,
-//     });
-//   });
-
-//   return <div ref={ref} id="map" />;
-// }
-
-
-// function MyStreetViewComponent({
-//   position,
-//   heading,
-//   pitch,
-// }: {
-//   position: google.maps.LatLngLiteral;
-//   heading: number;
-//   pitch: number;
-// }) {
-//   const ref: any = useRef();
-
-//   useEffect(() => {
-//     new window.google.maps.StreetViewPanorama(ref.current, {
-//       position: position,
-//       pov: {
-//         heading: heading,
-//         pitch: pitch,
-//       }
-//     });
-//   });
-
-//   return <div ref={ref} id="pano" />;
-// }
-
-
-// ############################################ Picking Phase ############################################
-// ############################################ Picking Phase ############################################
-// ############################################ Picking Phase ############################################
-// ############################################ Picking Phase ############################################
-// ############################################ Picking Phase ############################################
-// ############################################ Picking Phase ############################################
-
 
 let updated = false;
 let map;
@@ -101,14 +49,16 @@ let panorama;
 let marker;
 let round_number: number;
 let selectedLocation: any;
-function MyMapStreetComponent({
+function MyMapStreetComponentGuess({
   fenway,
   round,
-  pickingTime
+  pickingTime,
+  correctLocation
 }: {
   fenway: google.maps.LatLngLiteral;
   round: number;
   pickingTime: any;
+  correctLocation: google.maps.LatLngLiteral;
 }) {
   const refMap: any = useRef();
   const refPano: any = useRef();
@@ -117,28 +67,28 @@ function MyMapStreetComponent({
 
   useEffect(() => {
     if (!updated) {
-      map = new window.google.maps.Map(refMap.current, {
-        center: fenway,
-        zoom: 2,
-        panControl: false,
-        zoomControl: true,
-        fullscreenControl: false,
-        // streetViewControl: false
-      });
-      panorama = new window.google.maps.StreetViewPanorama(refPano.current, {
-        position: fenway,
-        pov: {
-          heading: 34,
-          pitch: 10,
-        },
-        linksControl: false,
-        panControl: false,
-        addressControl: false,
-        enableCloseButton: false,
-        zoomControl: true,
-        fullscreenControl: false,
-      });
-      map.setStreetView(panorama);
+        map = new window.google.maps.Map(refMap.current, {
+            center: fenway,
+            zoom: 2,
+            panControl: false,
+            zoomControl: true,
+            fullscreenControl: false,
+            streetViewControl: false
+        });
+
+        panorama = new window.google.maps.StreetViewPanorama(refPano.current, {
+            position: fenway,
+            pov: {
+                heading: 34,
+                pitch: 10,
+            },
+            linksControl: false,
+            panControl: false,
+            addressControl: false,
+            enableCloseButton: false,
+            zoomControl: true,
+            fullscreenControl: false,
+        });
     }
   });
 
@@ -176,7 +126,7 @@ function MyMapStreetComponent({
 }
 
 let stopCount: boolean = false;
-function CalculateTimeLeft(round, pickingTime) {
+function CalculateTimeLeftGuess(round, pickingTime) {
   round_number = round;
   const [seconds, setSeconds]: any = useState(round["round"]["pickingTime"] * 60);
   useEffect(() => {
@@ -214,64 +164,14 @@ function CalculateTimeLeft(round, pickingTime) {
   );
 }
 
-export function RenderMapStreet(round_number, pickingTime) {
+export function RenderMapStreetGuess(round_number, pickingTime, correctLocation) {
   const fenway = { lat: -31.55542202732198, lng: -54.54408893196694 };
   return (
     <div>
     <Wrapper apiKey="AIzaSyDaopI6hRGw8i5DlhA5lAiCIuZ-qoBH3AE" render={render}>
-      <MyMapStreetComponent fenway={fenway} round={round_number} pickingTime={pickingTime}/>
-      <CalculateTimeLeft round={round_number} pickingTime={pickingTime}/>
+      <MyMapStreetComponentGuess fenway={fenway} round={round_number} pickingTime={pickingTime} correctLocation={correctLocation}/>
+      <CalculateTimeLeftGuess round={round_number} pickingTime={pickingTime}/>
     </Wrapper>
     </div>
   );
 }
-
-
-// ############################################ Guessing Phase ############################################
-// ############################################ Guessing Phase ############################################
-// ############################################ Guessing Phase ############################################
-// ############################################ Guessing Phase ############################################
-// ############################################ Guessing Phase ############################################
-// ############################################ Guessing Phase ############################################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export function RenderMap() {
-//   const center = { lat: -34.397, lng: 150.644 };
-//   const zoom = 4;
-//   return (
-//     <Wrapper apiKey="AIzaSyDaopI6hRGw8i5DlhA5lAiCIuZ-qoBH3AE" render={render}>
-//       <MyMapComponent center={center} zoom={zoom} />
-//     </Wrapper>
-//   );
-// }
-
-// export function RenderStreetView() {
-//   const position = { lat: 42.345573, lng: -71.098326 };
-//   const heading = 34;
-//   const pitch = 10;
-//   return (
-//     <Wrapper apiKey="AIzaSyDaopI6hRGw8i5DlhA5lAiCIuZ-qoBH3AE" render={render}>
-//       <MyStreetViewComponent position={position} heading={heading} pitch={pitch} />
-//     </Wrapper>
-//   );
-// }
--- geo-duel/src/components/match-room.tsx
+++ geo-duel/src/components/match-room.tsx
@@ -6,7 +6,8 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/analytics';
 import { getFirestore, doc, getDoc, getDocFromServer } from "firebase/firestore"
 import { setDoc, addDoc, updateDoc, deleteDoc, deleteField, collection, query, where, onSnapshot, getDocs } from "firebase/firestore";
-import { RenderMap, RenderMapStreet, RenderStreetView } from '../components/map';
+import { RenderMapStreet } from '../components/map';
+import { RenderMapStreetGuess } from '../components/guess';
 
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
@@ -38,6 +39,11 @@ const params = new URLSearchParams(search);
 const roomName = String(params.get('room'));
 const playerName = String(params.get('player'));
 
+let currentRound = 1;
+let pickingStage = true;
+let guessingStage = false;
+let resultsStage = false;
+
 export function MatchRoom() {
     const [roomValues, setRoomValues] = useState<Room>({
         players: 0,
@@ -53,9 +59,14 @@ export function MatchRoom() {
         const docData: any = doc.data();
         const playersConnected = Object.keys(docData?.playersInfo).length;
 
-        // Object.keys(docData).forEach(function(key) {
-        //     console.log(key, docData[key]);
-        //   });
+        let count_players_picking = 0
+        Object.keys(docData['playersInfo']).forEach(function(key) {
+            if (`${currentRound}` in docData['playersInfo'][key]) {
+                if ('picking' in docData['playersInfo'][key][`${currentRound}`]) {
+                    count_players_picking += 1;
+                }
+            }
+        });
 
         if (playersConnected != playersPreviouslyConnected) {
             const values: Room = {
@@ -68,14 +79,26 @@ export function MatchRoom() {
     
             setRoomValues(values);
         }
+        else if (count_players_picking == docData.players && pickingStage == true) {
+            const values: Room = {
+                players: docData?.players,
+                places: docData?.places,
+                pickingTime: docData?.pickingTime,
+                guessingTime: docData?.guessingTime,
+                playersInfo: docData?.playersInfo
+            }
+    
+            setRoomValues(values);
+            guessingStage = true;
+            pickingStage = false;
+        }
     });
 
     if (Object.keys(roomValues.playersInfo).length < roomValues.players) {
         let playersNames: Array<string> = []
         Object.keys(roomValues.playersInfo).forEach(function(key) {
-            // console.log(key, roomValues.playersInfo[key]);
             playersNames.push(key);
-          });
+        });
 
         return (
             <div className="container">
@@ -91,21 +114,27 @@ export function MatchRoom() {
             </div>
         )
     }
-    else if (Object.keys(roomValues.playersInfo).length > roomValues.players) {
+    else if ((Object.keys(roomValues.playersInfo).length > roomValues.players) && (pickingStage == true)) {
         return (
             <div>
-                {/* <RenderMap />
-                <RenderStreetView /> */}
-                <RenderMapStreet round_number={1} pickingTime={roomValues.pickingTime} />
+                <RenderMapStreet round_number={`${currentRound}`} pickingTime={roomValues.pickingTime} />
+            </div>
+        )
+    }
+    else if ((Object.keys(roomValues.playersInfo).length > roomValues.players) && (guessingStage == true)) {
+        return (
+            <div>
+                <RenderMapStreetGuess round_number={`${currentRound}`} pickingTime={roomValues.guessingTime} correctLocation={{ lat: -31.55542202732198, lng: -54.54408893196694 }} />
+                {/* <RenderMap /> */}
+                {/* <RenderStreetView /> */}
+                {/* <RenderMapStreet round_number={1} pickingTime={roomValues.pickingTime} /> */}
             </div>
         )
     }
     else {
         return (
             <div>
-                <RenderMap />
-                {/* <RenderStreetView /> */}
-                {/* <RenderMapStreet round_number={1} pickingTime={roomValues.pickingTime} /> */}
+                <h2>Loading...</h2>
             </div>
         )
     }
